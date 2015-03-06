528f30e3e6e522dd682c7ea528c94f5ae193f4c5
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2015-03-06 16:34:47 +0100

    Creating a spinning wheel behaviour simulator

---
+++ spinwheel/test.py
@@ -0,0 +1,43 @@
+import spinwheel
+import numpy as np
+import matplotlib.pyplot as plot
+
+# Create a given spinning wheel
+static_friction = 0.1
+dynamic_friction = 0.01
+wheel = spinwheel.SpinWheel(static_friction, dynamic_friction)
+
+# Craft a simulated behaviour
+iterations = 200
+times = np.arange(iterations)
+wheelspeed = np.zeros(iterations)
+torque_input = np.zeros(iterations)
+
+period = 40
+for i in range(period):
+    torque_input[i] = np.sin(float(i/period * 3.14))
+
+for i in range(period):
+    torque_input[period+i] = -np.sin(float(i/period * 3.14))
+
+for i in range(period):
+    torque_input[2*period+i] = 0.4
+
+# Simulate
+for time in times:
+    # Feed the wheel with an input
+    wheel.update(torque_input[time])
+
+    # Record the subsequent speed
+    wheelspeed[time] = wheel.getCurrentWheelSpeed()
+
+# Plot this stuff
+plot.figure()
+plot.plot(times, wheelspeed, label='wheelspeed')
+plot.plot(times, torque_input, label='inputs')
+plot.legend()
+plot.grid()
+plot.show()
+
+
+
