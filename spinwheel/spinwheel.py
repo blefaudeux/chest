22480ff1e6973d531b4e6795bc8d6f92b351612c
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2015-03-18 10:59:40 +0100

    Added a spinning wheel with a power transfer function

--- spinwheel/spinwheel.py
+++ spinwheel/spinwheel.py
@@ -12,13 +12,40 @@ class SpinWheel:
         sign = np.sign(self._currentSpeed)
 
         # Prevent numerical instability
-        if np.abs(self._currentSpeed) < self._staticFriction:
-            current_static_friction = 0
-        else:
-            current_static_friction = self._staticFriction
+        overall_friction = (self._dynamicFriction * np.abs(self._currentSpeed) + self._staticFriction )
+        overall_friction = min(np.abs(self._currentSpeed), overall_friction)
 
-        self._currentSpeed += input_torque - sign * (self._dynamicFriction *
-                                                     np.abs(self._currentSpeed) + current_static_friction)
+        self._currentSpeed += input_torque - sign * overall_friction
+
+    def getCurrentWheelSpeed(self):
+        return self._currentSpeed
+
+    def setCurrentWheelSpeed(self):
+        return self._currentSpeed
+
+
+# Same principle, but using a power transfer fonction
+class SpinWheelPower:
+    def __init__(self, static_friction, dynamic_friction, target_speed):
+        self._inertia = 1                           # Not used, could be played with afterwards
+        self._staticFriction = static_friction      # The friction of the wheel against its axe at low speed
+        self._dynamicFriction = dynamic_friction    # The friction dependent to the speed
+        self._currentSpeed = 0                      # Current state of the wheel
+        self._targetSpeed = target_speed
+
+    def update(self, input_torque):
+        sign = np.sign(self._currentSpeed)
+
+        # Prevent numerical instability
+        overall_friction = (self._dynamicFriction * np.abs(self._currentSpeed) + self._staticFriction )
+        overall_friction = min(np.abs(self._currentSpeed), overall_friction)
+
+        # Modulate the input torque by the transfer function
+        delta_speed = self._currentSpeed - self._targetSpeed
+        sigma_speed = self._targetSpeed / 2
+        input_power = input_torque * np.exp(-delta_speed * delta_speed / (2 * sigma_speed * sigma_speed))
+
+        self._currentSpeed += input_power - sign * overall_friction
 
     def getCurrentWheelSpeed(self):
         return self._currentSpeed
--- spinwheel/test.py
+++ spinwheel/test.py
@@ -3,35 +3,33 @@ import numpy as np
 import matplotlib.pyplot as plot
 
 # Create a given spinning wheel
-static_friction = 0.1
-dynamic_friction = 0.01
-wheel = spinwheel.SpinWheel(static_friction, dynamic_friction)
+static_friction = 0.01
+dynamic_friction = 0.05
+wheel_power = spinwheel.SpinWheelPower(static_friction, dynamic_friction, 2)
 
 # Craft a simulated behaviour
-iterations = 300
+iterations = 100
 times = np.arange(iterations)
 wheelspeed = np.zeros(iterations)
+wheelspeed_power = np.zeros(iterations)
 torque_input = np.zeros(iterations)
 
 period = 40
 for i in range(period):
-    torque_input[i] = np.sin(float(i/period * 3.14))
-
-for i in range(period):
-    torque_input[3*period+i] = -np.sin(float(i/period * 3.14))
+    torque_input[i] = 0.7
 
 
 # Simulate
 for time in times:
     # Feed the wheel with an input
-    wheel.update(torque_input[time])
+    wheel_power.update(torque_input[time])
 
     # Record the subsequent speed
-    wheelspeed[time] = wheel.getCurrentWheelSpeed()
+    wheelspeed_power[time] = wheel_power.getCurrentWheelSpeed()
 
 # Plot this stuff
 plot.figure()
-plot.plot(times, wheelspeed, label='wheelspeed')
+plot.plot(times, wheelspeed_power, label='wheelSpeedPower')
 plot.plot(times, torque_input, label='inputs')
 plot.legend()
 plot.grid()
