97f7e5f62cfaa8a464406eaf461be8fd6c9e9c85
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2015-03-18 17:35:37 +0100

    working smooth spinning

--- spinwheel/spinwheel.py
+++ spinwheel/spinwheel.py
@@ -17,38 +17,88 @@ class SpinWheel:
 
         self._currentSpeed += input_torque - sign * overall_friction
 
-    def getCurrentWheelSpeed(self):
+    def get_current_speed(self):
         return self._currentSpeed
 
-    def setCurrentWheelSpeed(self):
+    def set_current_speed(self):
         return self._currentSpeed
 
 
-# Same principle, but using a power transfer fonction
-class SpinWheelPower:
-    def __init__(self, static_friction, dynamic_friction, target_speed):
-        self._inertia = 1                           # Not used, could be played with afterwards
-        self._staticFriction = static_friction      # The friction of the wheel against its axe at low speed
-        self._dynamicFriction = dynamic_friction    # The friction dependent to the speed
-        self._currentSpeed = 0                      # Current state of the wheel
-        self._targetSpeed = target_speed
+# Same principle, but using a manually defined curve shape
+class SmoothSpinning:
+    def __init__(self, rise_time, stop_time, brake_time):
+        self._rise_time = rise_time/2     # The characteristic time for the free wheel stop
+        self._stop_time = stop_time     # The characteristic time for the free wheel stop
+        self._brake_time = brake_time   # The characteristic time when 'braking' (reversed input)
+        self._currentSpeed = 0.
+        self._stopSpeed = 0.
+        self._brakeSpeed = 0.
+        self._timeStart = 0.
+        self._timeStop = 0.
+        self._timeBrake = 0.
+        self._stopThreshold = 0.01
 
-    def update(self, input_torque):
-        sign = np.sign(self._currentSpeed)
+    def sigmoid_update(self):
+        if (self._timeStop > 0. or self._timeBrake > 0.) and self._currentSpeed > 0.:
+            # We were in another procedure, start from here
+            self._timeStart = -np.log(max(0.01, -np.log(np.abs(self._currentSpeed)))) * self._rise_time
+            self._timeStart += 6*self._rise_time
 
-        # Prevent numerical instability
-        overall_friction = (self._dynamicFriction * np.abs(self._currentSpeed) + self._staticFriction )
-        overall_friction = min(np.abs(self._currentSpeed), overall_friction)
+        self._currentSpeed = 1/(1 + np.exp(-(self._timeStart - 6*self._rise_time)/self._rise_time))
+        self._timeStart += 1.
 
-        # Modulate the input torque by the transfer function
-        delta_speed = self._currentSpeed - self._targetSpeed
-        sigma_speed = self._targetSpeed / 2
-        input_power = input_torque * np.exp(-delta_speed * delta_speed / (2 * sigma_speed * sigma_speed))
+        # Reset the stop & brake procedures
+        self._timeStop = 0.
+        self._timeBrake = 0.
 
-        self._currentSpeed += input_power - sign * overall_friction
+    def sigmoid_stop(self):
+        if self._timeStop == 0.:
+            self._stopSpeed = self._currentSpeed
 
-    def getCurrentWheelSpeed(self):
+        self._currentSpeed = self._stopSpeed * np.exp(- self._timeStop / self._stop_time)
+
+        if np.abs(self._currentSpeed) < self._stopThreshold:
+            self._currentSpeed = 0.
+
+        self._timeStop += 1.
+
+    def sigmoid_brake(self):
+        if self._timeBrake == 0.:
+            self._brakeSpeed = self._currentSpeed
+
+        self._currentSpeed = self._brakeSpeed * np.exp(- self._timeBrake / self._brake_time)
+
+        if np.abs(self._currentSpeed) < self._stopThreshold:
+            self._currentSpeed = 0.
+
+        self._timeBrake += 1.
+
+    def update(self, direction):
+
+        if np.abs(self._currentSpeed) == 0.:
+            # Start a brand new sigmoid
+            self._timeStart = 0.
+            self.sigmoid_update()
+            self._currentSpeed *= np.sign(direction)
+
+        else:
+            sign = np.sign(self._currentSpeed)
+
+            if direction == 0.:
+                self.sigmoid_stop()
+
+            # Something is already in place
+            elif sign * np.sign(direction) > 0:
+                # Keep going with the existing sigmoid
+                self.sigmoid_update()
+                self._currentSpeed *= sign
+
+            else:
+                # Brake
+                self.sigmoid_brake()
+
+    def get_current_speed(self):
         return self._currentSpeed
 
-    def setCurrentWheelSpeed(self):
+    def set_current_peed(self):
         return self._currentSpeed
--- spinwheel/test.py
+++ spinwheel/test.py
@@ -3,33 +3,40 @@ import numpy as np
 import matplotlib.pyplot as plot
 
 # Create a given spinning wheel
-static_friction = 0.01
-dynamic_friction = 0.05
-wheel_power = spinwheel.SpinWheelPower(static_friction, dynamic_friction, 2)
+rise_time = 5
+stop_time = 5
+brake_time = 2
+
+wheel = spinwheel.SmoothSpinning(rise_time, stop_time, brake_time)
 
 # Craft a simulated behaviour
-iterations = 100
+iterations = 200
 times = np.arange(iterations)
 wheelspeed = np.zeros(iterations)
-wheelspeed_power = np.zeros(iterations)
 torque_input = np.zeros(iterations)
 
-period = 40
+period = int(iterations/10)
+for i in range(period*3):
+    torque_input[10+i] = 1
+
 for i in range(period):
-    torque_input[i] = 0.7
+    torque_input[period*4 + i] = 1
+
+for i in range(period):
+    torque_input[period*6 + i] = -1
 
 
 # Simulate
 for time in times:
     # Feed the wheel with an input
-    wheel_power.update(torque_input[time])
+    wheel.update(torque_input[time])
 
     # Record the subsequent speed
-    wheelspeed_power[time] = wheel_power.getCurrentWheelSpeed()
+    wheelspeed[time] = wheel.get_current_speed()
 
 # Plot this stuff
 plot.figure()
-plot.plot(times, wheelspeed_power, label='wheelSpeedPower')
+plot.plot(times, wheelspeed, label='wheel speed')
 plot.plot(times, torque_input, label='inputs')
 plot.legend()
 plot.grid()
