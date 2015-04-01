cd89f9e44f379808f488b90a2dbc0860e2d8db01
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2015-04-01 14:11:17 +0200

    Fixing the quoting issue

--- rotate_lbp_cascade/rotate_lbp.py
+++ rotate_lbp_cascade/rotate_lbp.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 
 import xml.etree.ElementTree as etree
 import numpy as np
@@ -13,31 +14,32 @@ def rotateLBPStage(inputTree):
             old_text = node.text
 
             # Rotate the node values
-            text = re.split(' |\n', old_text)
-            c = [item for item in text if (item != '' and item != '\n')]
+            parsedValues = re.split(' |\n', old_text)
+            c = [item for item in parsedValues if (item != '' and item != '\n')]
 
-            # Naive rotation, let's see whether this works..
             rotated_text = [c[0], c[1], c[2], c[5], c[6], c[7], c[8], c[9], c[10], c[3], c[4]]
+            # rotated_text = [c[0], c[1], c[2], c[3], c[4], c[5], c[6], c[7], c[8], c[9], c[10]]
 
             # Crush the values
             node.text = ""
-            for n in c:		# rotated_text
+            for n in rotated_text:
                 node.text += " " + str(n)
 
 
 def rotateLBPFeature(inputTree):
     width = int(inputTree.find("cascade/width").text)
-    height = int(inputTree.find("cascade/height").text)
+    # height = int(inputTree.find("cascade/height").text)
 
     features = inputTree.findall("cascade/features/_/rect")
 
     for rect in features:
         old_text = rect.text
-        text = re.split(' |\n', old_text)
-        c = [item for item in text if (item != '' and item != '\n')]
+        parsedValues = re.split(' |\n', old_text)
+        c = [item for item in parsedValues if (item != '' and item != '\n')]
 
         # The two first values are the rectangle initial coordinates, the two last its extend (width and height)
         rotated_text = [c[1], width-int(c[0])-int(c[2])-1, c[3], c[2]]
+        # rotated_text = [c[0], c[1], c[2], c[3]]
 
         # Crush the values
         rect.text = " "
@@ -53,5 +55,14 @@ rotateLBPFeature(tree)
 
 # Save the file (with the xml declaration)
 tree.write('lbpcascade_frontalface_rotated.xml', xml_declaration=True, encoding="utf-8")
+del tree
 
-# FIXME: ET saves the declaration with single quotes instead of double quotes
+# Fix an annoying ET bug : replace single quotes by double quotes in the saved file
+f = open('lbpcascade_frontalface_rotated.xml', 'r')
+text = f.read()
+f.close()
+
+f_fixed = open('lbpcascade_frontalface_rotated.xml', 'w')
+text = re.sub("\'", '"', text)
+f_fixed.write(text)
+f_fixed.close()
