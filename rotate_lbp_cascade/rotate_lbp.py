f25c6d17de2741f456d3dfd6540dcdc698f2573a
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2015-04-07 09:30:15 +0200

    cleaning up, fine with a patch within the LBP

--- rotate_lbp_cascade/rotate_lbp.py
+++ rotate_lbp_cascade/rotate_lbp.py
@@ -5,6 +5,8 @@ import numpy as np
 import re
 
 
+# Ref : http://w3facility.org/question/understanding-opencv-lbp-implementation/
+
 def rotateLBPStage(inputTree):
     stages = inputTree.findall("cascade/stages/_/weakClassifiers")
 
@@ -17,8 +19,22 @@ def rotateLBPStage(inputTree):
             parsedValues = re.split(' |\n', old_text)
             c = [item for item in parsedValues if (item != '' and item != '\n')]
 
-            rotated_text = [c[0], c[1], c[2], c[5], c[6], c[7], c[8], c[9], c[10], c[3], c[4]]
-            # rotated_text = [c[0], c[1], c[2], c[3], c[4], c[5], c[6], c[7], c[8], c[9], c[10]]
+            weights = [128, 64, 32, 16, 8, 4, 2, 1]
+
+            d = []  # We could check that the weight match here, we should have ints
+            d.append(int(c[5]) / weights[2] * weights[0])
+            d.append(int(c[6]) / weights[3] * weights[1])
+            d.append(int(c[7]) / weights[4] * weights[2])
+            d.append(int(c[8]) / weights[5] * weights[3])
+            d.append(int(c[9]) / weights[6] * weights[4])
+            d.append(int(c[10]) / weights[7]* weights[5])
+            d.append(int(c[3]) / weights[0] * weights[6])
+            d.append(int(c[4]) / weights[1] * weights[7])
+
+            d = [int(item) for item in d]
+
+            rotated_text = [c[0], c[1], c[2],
+                            str(d[0]), str(d[1]), str(d[2]), str(d[3]), str(d[4]), str(d[5]), str(d[6]), str(d[7])]
 
             # Crush the values
             node.text = ""
@@ -28,30 +44,37 @@ def rotateLBPStage(inputTree):
 
 def rotateLBPFeature(inputTree):
     width = int(inputTree.find("cascade/width").text)
-    # height = int(inputTree.find("cascade/height").text)
+    height = int(inputTree.find("cascade/height").text)
 
     features = inputTree.findall("cascade/features/_/rect")
 
+    # Change the LUT values
     for rect in features:
         old_text = rect.text
         parsedValues = re.split(' |\n', old_text)
         c = [item for item in parsedValues if (item != '' and item != '\n')]
 
         # The two first values are the rectangle initial coordinates, the two last its extend (width and height)
-        rotated_text = [c[1], width-int(c[0])-int(c[2])-1, c[3], c[2]]
-        # rotated_text = [c[0], c[1], c[2], c[3]]
+        rotated_text = [c[1], width-int(c[0])-3*int(c[2]), c[3], c[2]]
 
         # Crush the values
         rect.text = " "
         for n in rotated_text:
             rect.text += str(n) + " "
 
+    # Flip width and height values
+    width_ref = inputTree.findall("cascade/width")     # Get the handle first
+    width_ref[0].text = str(height)
+
+    height_ref = inputTree.findall("cascade/height")
+    height_ref[0].text = str(width)
+
 
 tree = etree.parse('lbpcascade_frontalface.xml')
 
 # Rotate stages and features in place
-rotateLBPStage(tree)
 rotateLBPFeature(tree)
+# rotateLBPStage(tree)
 
 # Save the file (with the xml declaration)
 tree.write('lbpcascade_frontalface_rotated.xml', xml_declaration=True, encoding="utf-8")
