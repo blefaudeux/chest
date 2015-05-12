04cdff9c762f31ac51aba3edcd206391e8f2fde9
benjamin.lefaudeux@gmail.com <benjamin.lefaudeux@gmail.com>
2015-05-12 22:12:39 +0200

    initial addition of multiple cascade rotations

--- rotate_lbp_cascade/rotate_lbp.py
+++ rotate_lbp_cascade/rotate_lbp.py
@@ -4,7 +4,7 @@ import numpy as np
 import re
 
 
-def rotateLBPStage(inputTree):
+def rotateLBPStage(inputTree, direction='left'):
     stages = inputTree.findall("cascade/stages/_/weakClassifiers")
 
     for inner_tree in stages:
@@ -16,8 +16,21 @@ def rotateLBPStage(inputTree):
             text = re.split(' |\n', old_text)
             c = [item for item in text if (item != '' and item != '\n')]
 
-            # Naive rotation, let's see whether this works..
-            rotated_text = [c[0], c[1], c[2], c[5], c[7], c[10], c[4], c[9], c[3], c[6], c[8]]
+            rotated_text = []
+            if direction == 'left':
+                rotated_text = [c[0], c[1], c[2], c[5], c[7], c[10], c[4], c[9], c[3], c[6], c[8]]
+                
+            elif direction == 'right':
+                # to be fixed
+                rotated_text = [c[0], c[1], c[2], c[5], c[7], c[10], c[4], c[9], c[3], c[6], c[8]]
+                
+            elif direction == 'bottom_up':
+                # to be fixed
+                rotated_text = [c[0], c[1], c[2], c[5], c[7], c[10], c[4], c[9], c[3], c[6], c[8]]
+            
+            else :
+                print("This direction is not supported")
+            
 
             # Crush the values
             node.text = ""
@@ -25,7 +38,7 @@ def rotateLBPStage(inputTree):
                 node.text += " " + str(n)
 
 
-def rotateLBPFeature(inputTree):
+def rotateLBPFeature(inputTree, direction = 'left'):
     width = int(inputTree.find("cascade/width").text)
     height = int(inputTree.find("cascade/height").text)
 
@@ -37,7 +50,18 @@ def rotateLBPFeature(inputTree):
         c = [item for item in text if (item != '' and item != '\n')]
 
         # The two first values are the rectangle initial coordinates, the two last its extend (width and height)
-        rotated_text = [c[1], width-int(c[0])-int(c[2]), c[3], c[2]]
+        rotated_text = []
+        if direction == 'left':
+            rotated_text = [c[1], width-int(c[0])-int(c[2]), c[3], c[2]]
+
+        elif direction == 'right':
+            rotated_text = [c[1], width-int(c[0])-int(c[2]), c[3], c[2]]
+
+        elif direction == 'bottom_up':
+            rotated_text = [c[1], width-int(c[0])-int(c[2]), c[3], c[2]]
+
+        else:
+            print("This direction is not supported")
 
         # Crush the values
         rect.text = " "
@@ -45,12 +69,16 @@ def rotateLBPFeature(inputTree):
             rect.text += str(n) + " "
 
 
+# Load the original xml file
 tree = etree.parse('lbpcascade_frontalface.xml')
 
 # Rotate stages and features in place
-rotateLBPStage(tree)
-rotateLBPFeature(tree)
-
-tree.write('rotated_cascade.xml')
-
-
+directions = ['left', 'right', 'bottom_up']
+for direction in directions :
+    print("Rotating cascade to the " + direction)
+    rotateLBPStage(tree, direction)
+    rotateLBPFeature(tree, direction)
+    filename = 'rotated_cascade_' + direction + '.xml'
+    tree.write(filename)
+    print(".." + filename + " saved\n")
+    
