d518869fba52279cee13d5288ab95a4feb7b139a
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2015-12-08 14:28:38 +0100

    adjusting the split script

--- snippets/split_pict.sh
+++ snippets/split_pict.sh
@@ -15,11 +15,14 @@ else
     sizey=720
 fi
 
+mkdir -p split/0
+mkdir -p split/1
+
 for f in *.jpg
 do
     filename="${f%.*}"
     echo "Splitting $f in $sizex x $sizey"
-    convert -crop ${sizex}x$sizey $f split/${filename}_%d.jpg
+    convert -crop ${sizex}x$sizey $f split/%d/$filename.jpg
 done
 
 echo "Done"
