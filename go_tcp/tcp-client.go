52feed744c00a4f8708a8f638c0a70f47b15bbff
benjamin.lefaudeux@gmail.com <benjamin.lefaudeux@gmail.com>
2016-02-07 23:03:49 +0100

    testing some go socket stuff

--- go_tcp/tcp-client.go
+++ go_tcp/tcp-client.go
@@ -1,26 +1,23 @@
 package main
 
-import "net"
-import "fmt"
-import "bufio"
-import "os"
+import "encoding/json"
+
+import (
+	"github.com/ARolek/jsonpipe"
+)
+
+type Message struct {
+	Name string
+	Body string
+	Time int64
+}
 
 func main() {
-  // connect to this socket
-  conn, _ := net.Dial("tcp", "127.0.0.1:8081")
+	jsonpipe.Handle("MyAction", myHandler)
+	jsonpipe.ListenAndServe(":8081")
+}
 
-  for {
-    // read in input from stdin
-    reader := bufio.NewReader(os.Stdin)
-    fmt.Print("Text to send: ")
-    text, _ := reader.ReadString('\n')
-
-    // send to socket
-    fmt.Fprintf(conn, text + "\n")
-
-    // listen for reply
-    message, _ := bufio.NewReader(conn).ReadString('\n')
-
-    fmt.Print("Message from server: "+message)
-  }
-} // - See more at: https://systembash.com/a-simple-go-tcp-server-and-tcp-client/#sthash.4d2TFKuc.dpuf
+func myHandler(data *json.RawMessage) (map[string]interface{}, error) {
+	json.Unmarshal(data, reply)
+	return reply
+}
